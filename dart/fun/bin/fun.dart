338a767754ef454322fcb50fa0740d95dfd053bb
Louis Liu <lusinga@126.com>
2022-11-19 23:29:13 +0800

    update

---
+++ dart/fun/bin/fun.dart
@@ -0,0 +1,5 @@
+import 'package:fun/fun.dart' as fun;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fun.calculate()}!');
+}
---
+++ dart/fun/lib/fun.dart
@@ -0,0 +1,7 @@
+int calculate() {
+  var l1 = List.empty(growable: true);
+  l1.add('测试下中文');
+  l1.add(2);
+  print(l1);
+  return 6 * 7;
+}
