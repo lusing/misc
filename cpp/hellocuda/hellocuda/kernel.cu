355135f5fe134216b5159c53e654bd2ec2aeef8d
Louis Liu <lusinga@126.com>
2023-04-24 20:44:22 +0800

    update

--- cpp/hellocuda/hellocuda/kernel.cu
+++ cpp/hellocuda/hellocuda/kernel.cu
@@ -23,32 +23,30 @@ int main(int argc, char** argv) {
     int devID;
     cudaDeviceProp props;
 
-    cudaDeviceProp deviceProp;
     int deviceCount;
     cudaError_t cudaError;
     cudaError = cudaGetDeviceCount(&deviceCount);
 
-    for (int i = 0; i < deviceCount; i++)
-    {
-        cudaError = cudaGetDeviceProperties(&deviceProp, i);
-
-        cout << "设备 " << i + 1 << " 的主要属性： " << endl;
-        cout << "设备显卡型号： " << deviceProp.name << endl;
-        cout << "设备全局内存总量（以MB为单位）： " << deviceProp.totalGlobalMem / 1024 / 1024 << endl;
-        cout << "设备上一个线程块（Block）中可用的最大共享内存（以KB为单位）： " << deviceProp.sharedMemPerBlock / 1024 << endl;
-        cout << "设备上一个线程块（Block）种可用的32位寄存器数量： " << deviceProp.regsPerBlock << endl;
-        cout << "设备上一个线程块（Block）可包含的最大线程数量： " << deviceProp.maxThreadsPerBlock << endl;
-        cout << "设备的计算功能集（Compute Capability）的版本号： " << deviceProp.major << "." << deviceProp.minor << endl;
-        cout << "设备上多处理器的数量： " << deviceProp.multiProcessorCount << endl;
+    if (cudaError == cudaSuccess) {
+        cout << "There are " << deviceCount << " cuda devices." << endl;
     }
 
-    // Get GPU information
-    //checkCudaErrors(cudaGetDevice(&devID));
-    //checkCudaErrors(cudaGetDeviceProperties(&props, devID));
-    //printf("Device %d: \"%s\" with Compute %d.%d capability\n", devID, props.name,
-    //    props.major, props.minor);
+    for (int i = 0; i < deviceCount; i++)
+    {
+        cudaError = cudaGetDeviceProperties(&props, i);
 
-    //printf("printf() is called. Output:\n\n");
+        if (cudaError == cudaSuccess) {
+            cout << "Device Name： " << props.name << endl;
+            cout << "Compute Capability version: " << props.major << "." << props.minor << endl;
+            cout << "设备上可用的全局内存总量:(G字节)" << props.totalGlobalMem / 1024 / 1024 / 1024 << endl;
+            cout << "时钟频率（以MHz为单位）:" << props.clockRate / 1000 << endl;
+            cout << "设备上多处理器的数量:" << props.multiProcessorCount << endl;
+            cout << "每个块的最大线程数:" << props.maxThreadsPerBlock <<endl;
+            cout << "内存总线宽度(位)" << props.memoryBusWidth << endl;
+            cout << "一个块的每个维度的最大尺寸:" << props.maxThreadsDim[0] << ","<< props.maxThreadsDim[1] << "," << props.maxThreadsDim[2] << endl;
+            cout << "一个网格的每个维度的最大尺寸:" << props.maxGridSize[0] << "," << props.maxGridSize[1] << "," << props.maxGridSize[2] <<endl;
+        }
+    }
 
     // Kernel configuration, where a two-dimensional grid and
     // three-dimensional blocks are configured.
